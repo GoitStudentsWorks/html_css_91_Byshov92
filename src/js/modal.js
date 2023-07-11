6a7a8110d4ab751eae14c3088493b55c3e0a0cbe
Anton Chan <106675165+chanchik96@users.noreply.github.com>
2023-07-11 19:18:54 +0300

    Close menu when open modal window

--- src/js/modal.js
+++ src/js/modal.js
@@ -11,9 +11,26 @@
   refs.openModalBtns.forEach(btn => {
     btn.addEventListener('click', toggleModal);
   });
+
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
+    const mediaQuery = window.matchMedia('(max-width: 767px)').matches;
+
     refs.modal.classList.toggle('is-hidden');
+
+    if (!refs.modal.classList.contains('is-hidden') && mediaQuery) {
+      closeMenu();
+    }
+  }
+
+  function closeMenu() {
+    const mobileMenu = document.querySelector('.js-menu-container');
+    const toggleMenuBtn = document.querySelector('.js-toggle-menu');
+
+    mobileMenu.classList.remove('is-open');
+    toggleMenuBtn.classList.remove('is-open');
+    toggleMenuBtn.setAttribute('aria-expanded', false);
+    document.body.style.overflow = 'auto';
   }
 })();
